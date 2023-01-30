2827cef418b52f166e3cf0b162879076d90c1626
Ankita Gupta <112807231+Ankita-k-codes@users.noreply.github.com>
2023-01-30 22:26:24 +0530

    Create Eko spoj.cpp

---
+++ Binary/Eko spoj.cpp
@@ -0,0 +1,59 @@
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool isPossibleSolution(int mid ,int arr[], int n , int target){
+   
+   int sum=0;
+
+    for(int i=0; i<n; i++){
+        int diff = 0;
+
+        if(arr[i] > mid)
+         diff = arr[i] - mid;  //mid = saw
+        
+        sum = sum + diff;
+    }
+
+    if(sum >= target)
+       return true;
+
+    else
+       return false;
+}
+int getMaxHeightofSaw(int arr[], int n,int target){
+    sort(arr,arr+n);          //sort to get max height
+    int s = 0;
+    int e = arr[n-1];
+    
+    int ans = -1;
+
+    while(s<=e){
+        int mid = s + (e-s)/2;
+
+        if(isPossibleSolution(mid, arr, n, target)){
+
+            //store ans
+           ans = mid;
+
+           //move to right
+           s = mid + 1;
+        }
+
+        else{
+            //move to left
+            e = mid - 1;
+        }
+    }
+     return ans;
+}
+
+int main(){
+    int arr[] = {15,8,18,6};
+    int target = 30;
+
+    int ans = getMaxHeightofSaw(arr,4,target);
+    cout<<"Answer is "<< ans <<endl;
+    return 0;
+}
